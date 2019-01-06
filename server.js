804868f7bda05b2b27a8857f23463a3698856e26
Glitch (api-uziel-url-shortener) <none>
2019-01-06 01:40:58 +0000

    🚨🐴 Updated with Glitch

--- server.js
+++ server.js
@@ -12,9 +12,12 @@ var app = express();
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
+// Not sure but I added this:
+const assert = require('assert');
+
 /** this project needs a db !! **/ 
 // mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useMongoClient: true });
 
 app.use(cors());
 
@@ -28,18 +31,56 @@ app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-  
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+
+var Schema = mongoose.Schema;
+
+var url = new Schema({
+  original: {type: String, required: true},
+  short: {type: Number, unique: true}
 });
 
+var URL = mongoose.model('URL', url);
+var i = 0;
 
-/////////////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////////////
+
 app.route('/api/shorturl/new').post(function(req, res){
-  res.json({'original_url': req.body.url, 'short_url': 2});
+  
+  URL.findOne({original: req.body.url}, function(err, data){
+    
+    if(data == null) {
+      
+      console.log('none found');
+      
+      i += 1000;
+      
+      var newRecord = new URL({
+        original: req.body.url,
+        short: i
+      });
+      
+      console.log(newRecord);
+      
+      console.log('need to create one');
+      
+      newRecord.save(function(err, doc){
+        if (err) return console.log('ERROR');
+        console.log('creating a new record with: ', doc.original);
+        res.json({"Original_URL": doc.original, "Short_URL": doc.short});
+      });     
+      
+    }
+    
+    else{
+      console.log('found it');
+      res.json({"Original_URL": data.original, "Short_URL": data.short});
+    }
+
+    
+  });
+  
+  
 });
+
 
 
 app.listen(port, function () {
